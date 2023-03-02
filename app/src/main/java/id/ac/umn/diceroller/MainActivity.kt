225734ec3aa4f40f8dee4ccfbe835a6fef4b35b2
stefan5035 <stefan.madethen@gmail.com>
2023-03-02 11:27:16 +0700

    Clean Code

--- app/src/main/java/id/ac/umn/diceroller/MainActivity.kt
+++ app/src/main/java/id/ac/umn/diceroller/MainActivity.kt
@@ -4,22 +4,38 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //rollButton for rolling dice
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
-            //test
+            rollButton.setOnClickListener { rollDice() }
         }
     }
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
 }
+
 class Dice(private val numSides: Int) {
     fun roll(): Int {
-
+        // returns one number between one to six
         return (1..numSides).random()
     }
 }
