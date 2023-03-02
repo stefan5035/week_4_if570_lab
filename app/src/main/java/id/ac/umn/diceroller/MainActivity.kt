5e777b4eecf371fb56e13a93fb377203293ca3af
stefan5035 <stefan.madethen@gmail.com>
2023-03-02 10:58:10 +0700

    Styling and Dice class

---
+++ app/src/main/java/id/ac/umn/diceroller/MainActivity.kt
@@ -0,0 +1,23 @@
+package id.ac.umn.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
+        }
+    }
+}
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
