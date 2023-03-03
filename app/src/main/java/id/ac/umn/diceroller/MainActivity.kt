f1a37ac28d6c260df526de973cf1098c9174f6c1
stefan5035 <stefan.madethen@gmail.com>
2023-03-04 00:03:23 +0700

    Displaying the right image

--- app/src/main/java/id/ac/umn/diceroller/MainActivity.kt
+++ app/src/main/java/id/ac/umn/diceroller/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         //rollButton for rolling dice
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val diceImage: ImageView = findViewById(R.id.imageView)
             rollButton.setOnClickListener { rollDice() }
         }
     }
@@ -23,14 +22,25 @@ class MainActivity : AppCompatActivity() {
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
+    // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
-
-        // Update the screen with the dice roll
+        // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
-        diceImage.setImageResource(R.drawable.dice_2)
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
 
+        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
